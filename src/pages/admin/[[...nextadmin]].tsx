48216b18d7a0f97b94e3e0501667df8a7b4146a1
Florian Geierstanger <florian.geierstanger@codecentric.de>
2024-02-29 09:02:19 +0100

    set basePath

--- src/pages/admin/[[...nextadmin]].tsx
+++ src/pages/admin/[[...nextadmin]].tsx
@@ -1,10 +1,18 @@
 import { type GetServerSideProps, type GetServerSidePropsResult } from "next";
-import { NextAdmin, type AdminComponentProps } from "@premieroctet/next-admin";
+import {
+  NextAdmin,
+  type AdminComponentProps,
+  type NextAdminOptions,
+} from "@premieroctet/next-admin";
 import schema from "../../../prisma/json-schema/json-schema.json"; // import the json-schema.json file
 import "@premieroctet/next-admin/dist/styles.css";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+const options: NextAdminOptions = {
+  basePath: "/admin",
+};
 
 export default function Admin(props: AdminComponentProps) {
   return <NextAdmin {...props} />;
@@ -14,7 +22,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const { nextAdminRouter } = await import(
     "@premieroctet/next-admin/dist/router"
   );
-  const adminRouter = await nextAdminRouter(prisma, schema);
+  const adminRouter = await nextAdminRouter(prisma, schema, options);
   return adminRouter.run(req, res) as Promise<
     GetServerSidePropsResult<Record<string, any>>
   >;
